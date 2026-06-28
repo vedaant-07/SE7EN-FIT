31f0c5fac47cb3bdde75cd075d3c289943dacd50
vedaant-07 <se7enfits.07@gmail.com>
2026-06-28 19:07:24 +0530

    Load health tracking entity routes

--- server/start.js
+++ server/start.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import './lib/supabaseQueryCatchPatch.js';
 import './routes/gymOwnerPreload.js';
+import './routes/healthEntityPreload.js';
 import './routes/entityCompatPreload.js';
 import './routes/adminExtraPreload.js';
 import './index.production.js';
