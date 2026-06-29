6e034723f36ecf4a38daee74f8f7177c91e6254c
vedaant-07 <se7enfits.07@gmail.com>
2026-06-29 19:17:40 +0530

    Connect owner dashboard v2

--- src/pages/GymOwnerDashboard.jsx
+++ src/pages/GymOwnerDashboard.jsx
@@ -1,5 +1,3 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { base44 } from '@/api/base44Client';
-import LoadingScreen from '@/components/se7enfit/LoadingScreen';
-import MediaUploadCropper from '@/components/se7enfit/MediaUploadCropper';
+import DashboardV2 from "./GymOwnerDashboardV2.jsx";
+
+export default DashboardV2;
