ca757bff04cc7b0cd24a8ac7e245fa25e297b89a
vedaant-07 <se7enfits.07@gmail.com>
2026-08-02 01:03:22 +0530

    Enforce verified admin sessions on billing routes

--- server/start.js
+++ server/start.js
@@ -13,6 +13,7 @@ import './routes/gymBattlesPreload.js';
 import './routes/platformIntegrationPreload.js';
 import './routes/platformPhase3Preload.js';
 import './routes/platformPhase3CorrectionsPreload.js';
+import './routes/adminSessionGuardPreload.js';
 import './routes/billingPhase4Preload.js';
 import './routes/adminExtraPreload.js';
 import './index.production.js';
