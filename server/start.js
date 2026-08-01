328e1a9063be3db676271b5894c14be7e9c02be7
vedaant-07 <se7enfits.07@gmail.com>
2026-08-02 01:48:45 +0530

    Verify Phase 5 routes after registration

--- server/start.js
+++ server/start.js
@@ -16,6 +16,7 @@ import './routes/platformPhase3CorrectionsPreload.js';
 import './routes/adminSessionGuardPreload.js';
 import './routes/billingPhase4CorrectionsPreload.js';
 import './routes/billingPhase4Preload.js';
+import './routes/memberProductPhase5CorrectionsPreload.js';
 import './routes/memberProductPhase5Preload.js';
 import './routes/adminExtraPreload.js';
 import './index.production.js';
